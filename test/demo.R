bc9acdcfe05c205066c7e714f45d360230e77343
xieguigang <xie.guigang@gcmodeller.org>
2021-10-23 17:07:15 +0800

    Create demo.R

---
+++ test/demo.R
@@ -0,0 +1,18 @@
+# # H1 title
+# 
+# this is an example of write R# notebook document, just like write 
+# a regular R# script at here.
+# 
+# ## demo R code block in notebook
+# 
+# just write a markdown code block at here, the code that show below
+# will not run when generate R notebook html document view:
+#
+# ```r
+# # demo of R# code
+# const [x, y] as integer = [5, 5];
+# 
+# print([x, y]);
+# # [2] 5 5
+# ```
+

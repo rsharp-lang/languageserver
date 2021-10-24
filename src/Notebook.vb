fcd7440dee4ddfea42015da333359a2d442b7834
xieguigang <xie.guigang@gcmodeller.org>
2021-10-24 16:22:43 +0800

    parse notebook model

--- src/Notebook.vb
+++ src/Notebook.vb
@@ -1,7 +1,6 @@
-﻿
-Imports SMRUCC.Rsharp.Interpreter
+﻿Imports Microsoft.VisualBasic.Language
 Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine
-Imports SMRUCC.Rsharp.Language.TokenIcer
+Imports SMRUCC.Rsharp.Interpreter.ExecuteEngine.ExpressionSymbols
 Imports SMRUCC.Rsharp.Runtime.Components
 
 ''' <summary>
@@ -9,9 +8,56 @@ Imports SMRUCC.Rsharp.Runtime.Components
 ''' </summary>
 Public Class Notebook
 
+    Public Property blocks As NoteBlock()
+
     Public Shared Function fromRscript(rscript As Rscript) As Notebook
         Dim lines As Expression() = Expression.ParseLines(rscript, keepsCommentLines:=True).ToArray
+        Dim blockList As New List(Of NoteBlock)
+        Dim regionMark As Boolean = False
+        Dim codeBlock As New List(Of Expression)
 
-        Return New Notebook
+        For Each line As Expression In lines
+            If TypeOf line Is CodeComment Then
+                Select Case DirectCast(line, CodeComment).CommentAnnotation
+                    Case Annotations.BlockComment, Annotations.LineComment
+                        blockList += New MarkdownBlock With {
+                            .markdown = DirectCast(line, CodeComment).comment
+                        }
+                    Case Annotations.RegionStart
+                        regionMark = True
+                    Case Annotations.EndRegion
+                        regionMark = False
+                        blockList += New RCodeBlock With {
+                            .block = codeBlock.ToArray
+                        }
+                End Select
+            ElseIf regionMark Then
+                codeBlock += line
+            Else
+                blockList += New RCodeBlock With {
+                    .block = {line}
+                }
+            End If
+        Next
+
+        Return New Notebook With {
+            .blocks = blockList.ToArray
+        }
     End Function
+End Class
+
+Public MustInherit Class NoteBlock
+
+End Class
+
+Public Class MarkdownBlock : Inherits NoteBlock
+
+    Public Property markdown As String
+
+End Class
+
+Public Class RCodeBlock : Inherits NoteBlock
+
+    Public Property block As Expression()
+
 End Class

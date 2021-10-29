8e78f77b88c01c3034454bff8be314c2dd5c102d
xieguigang <xie.guigang@gcmodeller.org>
2021-10-30 02:06:27 +0800

    rendering demo

---
+++ app/hello_world.R
@@ -0,0 +1,27 @@
+# # Demo test for markdown
+# This is a paragraph in the markdown document.
+# ## H2 title
+# This is another paragraph in the markdown document.
+# ### DEMO1: Markdown list
+# Here is a list of supported features in R# notebook:
+#
+# + markdown document
+# + code block
+# + image output of the code block
+#
+# ### DEMO2: Table test
+# Create a table in document just like what you do in the markdown document:
+#
+# |Features                      |supported|
+# |------------------------------|---------|
+# |markdown document             |yes      |
+# |code block                    |yes      |
+# |image output of the code block|yes      |
+#
+# ### DEMO3: Code test
+# Yes, you can write a code block in markdown document block:
+# ```r
+# print("Hello world!");
+# ```
+# > But this code block will not be evaluated
+;

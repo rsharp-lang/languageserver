d04f3b712100dda9f11b28bd93b82ab032cd5d1e
xieguigang <xie.guigang@gcmodeller.org>
2021-10-30 01:19:36 +0800

    add cli help

--- app/renderHtml.R
+++ app/renderHtml.R
@@ -1,7 +1,18 @@
-const notebook as string = ?"--notebook" || stop("no notebook source file is specified!");
-const htmlfile as string = ?"--output" || `${dirname(notebook)}/${basename(notebook)}.html`;
-
 require(Rnotebook);
+
+#' Rendering R# notebook script
+#'
+#' @author xieguigang <xie.guigang@gcmodeller.org>
+#' @description A cli pipeline R# script for run rendering 
+#'    of the R# notebook script into html help document.
+#' 
+
+[@info "The R# source script file its filepath."]
+[@type "filepath"]
+const notebook as string = ?"--notebook" || stop("no notebook source file is specified!");
+[@info "The output file path of the html document file that rendered from the given R# source script file."]
+[@type "filepath"]
+const htmlfile as string = ?"--output" || `${dirname(notebook)}/${basename(notebook)}.html`;
 
 engine::parse(notebook)
 |> pipHtml

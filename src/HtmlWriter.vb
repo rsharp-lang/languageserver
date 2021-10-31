4f26272cc0020ad8d0652e0358bcab6acdf0f796
xieguigang <xie.guigang@gcmodeller.org>
2021-10-31 15:46:57 +0800

    update source file banners

--- src/HtmlWriter.vb
+++ src/HtmlWriter.vb
@@ -1,4 +1,46 @@
-﻿Imports System.Text
+﻿#Region "Microsoft.VisualBasic::e90afbdb814b02098b94f28856f57d9c, Rnotebook\src\HtmlWriter.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xie (genetics@smrucc.org)
+    '       xieguigang (xie.guigang@live.com)
+    ' 
+    ' Copyright (c) 2018 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+
+    ' /********************************************************************************/
+
+    ' Summaries:
+
+    ' Class HtmlWriter
+    ' 
+    '     Constructor: (+1 Overloads) Sub New
+    '     Function: GetHtml
+    ' 
+    ' /********************************************************************************/
+
+#End Region
+
+Imports System.Text
 Imports Microsoft.VisualBasic.MIME.text.markdown
 Imports Microsoft.VisualBasic.Text.Xml
 Imports SMRUCC.Rsharp.Runtime
@@ -40,3 +82,4 @@ Public Class HtmlWriter
         Return html.ToString
     End Function
 End Class
+
--- src/NoteBlock.vb
+++ src/NoteBlock.vb
@@ -1,4 +1,57 @@
-﻿Imports System.Text
+﻿#Region "Microsoft.VisualBasic::49057a693ea71c8010345203833d3a2a, Rnotebook\src\NoteBlock.vb"
+
+    ' Author:
+    ' 
+    '       asuka (amethyst.asuka@gcmodeller.org)
+    '       xie (genetics@smrucc.org)
+    '       xieguigang (xie.guigang@live.com)
+    ' 
+    ' Copyright (c) 2018 GPL3 Licensed
+    ' 
+    ' 
+    ' GNU GENERAL PUBLIC LICENSE (GPL3)
+    ' 
+    ' 
+    ' This program is free software: you can redistribute it and/or modify
+    ' it under the terms of the GNU General Public License as published by
+    ' the Free Software Foundation, either version 3 of the License, or
+    ' (at your option) any later version.
+    ' 
+    ' This program is distributed in the hope that it will be useful,
+    ' but WITHOUT ANY WARRANTY; without even the implied warranty of
+    ' MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    ' GNU General Public License for more details.
+    ' 
+    ' You should have received a copy of the GNU General Public License
+    ' along with this program. If not, see <http://www.gnu.org/licenses/>.
+
+
+
+    ' /********************************************************************************/
+
+    ' Summaries:
+
+    ' Class NoteBlock
+    ' 
+    ' 
+    ' 
+    ' Class MarkdownBlock
+    ' 
+    '     Properties: markdown
+    ' 
+    '     Function: GetScript, ToHtml, ToString
+    ' 
+    ' Class RCodeBlock
+    ' 
+    '     Properties: block, region, regionName, text
+    ' 
+    '     Function: GetScript, ToHtml, ToString, Trim
+    ' 
+    ' /********************************************************************************/
+
+#End Region
+
+Imports System.Text
 Imports Microsoft.VisualBasic.ComponentModel.Ranges.Model
 Imports Microsoft.VisualBasic.Text
 Imports Microsoft.VisualBasic.Text.Xml

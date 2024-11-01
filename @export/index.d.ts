b6d661ef61069cc9635e2d2f169249b8cd9c5e5e
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-01 17:23:28 +0800

    run package msbuyild

---
+++ @export/index.d.ts
@@ -0,0 +1,14 @@
+﻿// export R# source type define for javascript/typescript language
+//
+// package_source=languageserver
+
+declare namespace languageserver {
+   module _ {
+      /**
+      */
+      function onLoad(): object;
+   }
+   /**
+   */
+   function pipHtml(rscript: any): object;
+}

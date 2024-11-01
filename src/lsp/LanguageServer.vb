0fc0a22638dd673bd4b15f54805a231334ac8174
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-01 21:58:56 +0800

    get resource

--- src/lsp/LanguageServer.vb
+++ src/lsp/LanguageServer.vb
@@ -4,6 +4,8 @@ Imports Flute.Http.Core
 Imports Flute.Http.Core.Message
 Imports Flute.Http.FileSystem
 Imports Microsoft.VisualBasic.ApplicationServices
+Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
+Imports Microsoft.VisualBasic.Emit.Delegates
 
 Public Class LanguageServer : Implements IAppHandler
 
@@ -11,7 +13,9 @@ Public Class LanguageServer : Implements IAppHandler
     ReadOnly http As HttpSocket
 
     Sub New(port As Integer, Optional vscode_clr As String = Nothing)
-        If vscode_clr.FileExists(ZERO_Nonexists:=True) Then
+        If vscode_clr.DirectoryExists Then
+            vscode = New FileSystem(vscode_clr)
+        ElseIf vscode_clr.FileExists(ZERO_Nonexists:=True) Then
             vscode = New FileSystem(FindResource(Assembly.LoadFrom(vscode_clr.GetFullPath)))
         End If
 
@@ -19,7 +23,26 @@ Public Class LanguageServer : Implements IAppHandler
     End Sub
 
     Private Shared Function FindResource(asm As Assembly) As IFileSystemEnvironment
+        Dim types As Type() = asm.GetTypes
 
+        For Each t_module As Type In types
+            Dim ptr As MethodInfo() = t_module.GetMethods(PublicShared)
+
+            For Each f As MethodInfo In ptr
+                If Not f.GetParameters.IsNullOrEmpty Then
+                    Continue For
+                End If
+                If Not f.GetGenericArguments.IsNullOrEmpty Then
+                    Continue For
+                End If
+
+                If f.ReturnType.ImplementInterface(Of IFileSystemEnvironment)() Then
+                    Return f.Invoke(Nothing, {})
+                End If
+            Next
+        Next
+
+        Throw New InvalidProgramException("no resource was provided!")
     End Function
 
     Private Sub AppHandler(request As HttpRequest, response As HttpResponse) Implements IAppHandler.AppHandler

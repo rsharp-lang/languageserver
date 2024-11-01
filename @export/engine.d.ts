8720aec436218bfcc566577fe5094a1969e5cad2
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-01 19:43:14 +0800

    run package msbuild

---
+++ @export/engine.d.ts
@@ -0,0 +1,23 @@
+﻿// export R# package module type define for javascript/typescript language
+//
+//    imports "engine" from "languageserver";
+//
+// ref=languageserver.RscriptEngine@languageserver, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
+
+/**
+*/
+declare namespace engine {
+   /**
+     * @param port default value Is ``321``.
+   */
+   function listen(port?: object): ;
+   /**
+   */
+   function parse(handle: string): object;
+   /**
+     * @param style default value Is ``null``.
+     * @param strict default value Is ``false``.
+     * @param env default value Is ``null``.
+   */
+   function toHtml(nb: object, style?: string, strict?: boolean, env?: object): string;
+}
---
+++ man/languageserver/engine/engine.md
@@ -0,0 +1,7 @@
+﻿# engine
+
+
+
++ [toHtml](engine/toHtml.1) 
++ [parse](engine/parse.1) 
++ [listen](engine/listen.1) 

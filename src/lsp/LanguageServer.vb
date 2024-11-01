a073291233def0b00b78e9c3d89c8b18eecc28ee
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-01 23:28:58 +0800

    get function help information

--- src/lsp/LanguageServer.vb
+++ src/lsp/LanguageServer.vb
@@ -1,4 +1,6 @@
-﻿Imports System.Reflection
+﻿Imports System.IO
+Imports System.Reflection
+Imports System.Runtime.CompilerServices
 Imports Flute.Http
 Imports Flute.Http.Core
 Imports Flute.Http.Core.Message
@@ -6,12 +8,17 @@ Imports Flute.Http.FileSystem
 Imports Microsoft.VisualBasic.ApplicationServices
 Imports Microsoft.VisualBasic.ComponentModel.DataSourceModel
 Imports Microsoft.VisualBasic.Emit.Delegates
+Imports SMRUCC.Rsharp.Development
+Imports SMRUCC.Rsharp.Interpreter
+Imports SMRUCC.Rsharp.Runtime
+Imports SMRUCC.Rsharp.Runtime.Interop
 
 Public Class LanguageServer : Implements IAppHandler
 
     ReadOnly vscode As FileSystem
     ReadOnly http As HttpSocket
     ReadOnly cache As New Dictionary(Of String, String)
+    ReadOnly R As RInterpreter
 
     Sub New(port As Integer, Optional vscode_clr As String = Nothing)
         If vscode_clr.DirectoryExists Then
@@ -20,6 +27,8 @@ Public Class LanguageServer : Implements IAppHandler
             vscode = New FileSystem(FindResource(Assembly.LoadFrom(vscode_clr.GetFullPath)))
         End If
 
+        R = RInterpreter.Rsharp
+        R.RedirectOutput(New StreamWriter(Console.OpenStandardOutput), OutputEnvironments.Html)
         http = New HttpDriver() _
             .HttpMethod("get", Me) _
             .HttpMethod("post", AddressOf setDefault) _
@@ -63,11 +72,31 @@ Public Class LanguageServer : Implements IAppHandler
 
                 Call response.WriteHeader("text/html", bytes.Length)
                 Call response.Write(bytes)
+            Case "/lsp/get/symbol/"
+                Dim name = request.URL("symbol")
+                Dim key = request.URL("key")
+                Dim symbol As Object = R.Evaluate(name)
+                Dim docs = R.globalEnvir _
+                    .packages _
+                    .packageDocs
+
+                Using s As New MemoryStream
+                    R.globalEnvir.stdout.splitLogging(s)
+
+                    If TypeOf symbol Is RMethodInfo Then
+                        Call docs.PrintHelp(symbol, R.globalEnvir.stdout)
+                    ElseIf TypeOf symbol Is FunctionDeclare Then
+
+                    End If
+
+                    Call R.globalEnvir.stdout.Flush()
+                    Call s.Flush()
+                    Call s.Seek(Scan0, SeekOrigin.Begin)
+                    Call response.WriteHeader("text/html", s.Length)
+                    Call response.Write(s.ToArray)
+                End Using
             Case Else
-                If url.StartsWith("/get") Then
-                Else
-                    Call WebFileSystemListener.HostStaticFile(vscode, request, response)
-                End If
+                Call WebFileSystemListener.HostStaticFile(vscode, request, response)
         End Select
     End Sub
 
@@ -81,11 +110,12 @@ Public Class LanguageServer : Implements IAppHandler
                 SyncLock cache
                     cache(key) = post.Objects("doc")
                 End SyncLock
-            Case "/lsp/save"
+            Case "/lsp/save/"
                 cache(key).SaveTo(path:=post.Objects("file"))
         End Select
     End Sub
 
+    <MethodImpl(MethodImplOptions.AggressiveInlining)>
     Public Sub Listen()
         Call http.Run()
     End Sub

a0190d3925f6289105e76fb91528bb3c7717c508
この中二病に爆焔を！ <xie.guigang@gcmodeller.org>
2024-11-02 17:47:24 +0800

    run package msbuyukld

--- @export/engine.d.ts
+++ @export/engine.d.ts
@@ -5,19 +5,39 @@
 // ref=languageserver.RscriptEngine@languageserver, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null
 
 /**
+ * R# language server toolkit
+ * 
 */
 declare namespace engine {
    /**
-     * @param port default value Is ``321``.
+    * listen on the tcp port for run the lsp http service
+    * 
+    * 
+     * @param port -
+     * 
+     * + default value Is ``321``.
+     * @param vscode_clr 
+     * + default value Is ``null``.
    */
-   function listen(port?: object): ;
+   function listen(port?: object, vscode_clr?: string): ;
    /**
+    * Parse the given rscript as notebook rendering model
+    * 
+    * 
+     * @param handle -
    */
    function parse(handle: string): object;
    /**
-     * @param style default value Is ``null``.
-     * @param strict default value Is ``false``.
-     * @param env default value Is ``null``.
+    * 
+    * 
+     * @param nb -
+     * @param style the css style text
+     * 
+     * + default value Is ``null``.
+     * @param strict 
+     * + default value Is ``false``.
+     * @param env 
+     * + default value Is ``null``.
    */
    function toHtml(nb: object, style?: string, strict?: boolean, env?: object): string;
 }
--- man/languageserver/engine/engine.md
+++ man/languageserver/engine/engine.md
@@ -1,7 +1,7 @@
 ﻿# engine
 
-
+R# language server toolkit
 
 + [toHtml](engine/toHtml.1) 
-+ [parse](engine/parse.1) 
-+ [listen](engine/listen.1) 
++ [parse](engine/parse.1) Parse the given rscript as notebook rendering model
++ [listen](engine/listen.1) listen on the tcp port for run the lsp http service
